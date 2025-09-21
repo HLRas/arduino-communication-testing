d2c042629b3443abe31faa98c243d8e8a3ad87d3
HLRas <herman.ras2003@gmail.com>
2025-09-21 10:34:19 +0200

    fixed timing

--- testing.py
+++ testing.py
@@ -49,7 +49,7 @@ def thread():
                 except Exception as e:
                     print(f"[Python] Read error: {e}")
 
-            time.sleep(0.00001)
+            time.sleep(0.0001)
             
         except Exception as e:
             print(f"[Python] Failed: {e}")
